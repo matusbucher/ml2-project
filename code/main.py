8306e38789fa29867be25728d934771d56e0afee
matusbucher <bucher.matus@gmail.com>
2026-05-28 00:19:00 +0200

    Add visualize common rule CA

--- code/main.py
+++ code/main.py
@@ -1,6 +1,39 @@
 from data_generation import *
 from transformer import *
 
+from visualize import visualize_ca
+
+
+CA_RULE_30_SAVE_PATH = "results/rule_30.png"
+CA_RULE_90_SAVE_PATH = "results/rule_90.png"
+CA_RULE_110_SAVE_PATH = "results/rule_110.png"
+
+SHOW_CA_STEPS = 24
+SHOW_CA_ITERS = 9
+
+
+
+def show_ca(
+    rule_number: int,
+    n_steps: int = SHOW_CA_STEPS,
+    n_iters: int = SHOW_CA_ITERS,
+    random_seed: int | None = None,
+    save_path: str | None = None,
+) -> None:
+    """Visualizes the history of a cellular automaton with a given rule number and number of steps."""
+    
+    if random_seed is not None:
+        torch.manual_seed(random_seed)
+    
+    ca = CellularAutomaton(rule_number)
+    initial_states = torch.randint(0, 2, (n_iters, 32), dtype=torch.long)
+    visualize_ca(
+        ca=ca,
+        init_states=initial_states,
+        steps=n_steps,
+        save_path=save_path
+    )
+
 
 def first_experiment():
     """Runs the first experiment with rule number 110 and 1 step."""
@@ -49,4 +82,8 @@ def first_experiment():
 
 
 if __name__ == "__main__":
-    first_experiment()
+    show_ca(30, random_seed=42, save_path=CA_RULE_30_SAVE_PATH)
+    show_ca(90, random_seed=42, save_path=CA_RULE_90_SAVE_PATH)
+    show_ca(110, random_seed=42, save_path=CA_RULE_110_SAVE_PATH)
+
+    # first_experiment()
